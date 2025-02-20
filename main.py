7e57f7efb921748e9223a4b56502e0d9823e583f
cmooiman <cmooiman@outlook.com>
2025-02-20 22:32:36 +0100

    halfway through L5: Draw Player

--- main.py
+++ main.py
@@ -4,13 +4,28 @@ from constants import *
 
 def main():
     pygame.init()
+    
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    # Set up display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids!")
+    
+    # Main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # Fill the screen with a color
         screen.fill("black")
+        
+        # Update the display
         pygame.display.flip()
+        
+        # Cap the frame rate to 60 frames per second
+        dt = (clock.tick(60) / 1000)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")

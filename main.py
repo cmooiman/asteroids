409439ed918fd7039bfc519ab6af8c4952401a40
cmooiman <cmooiman@outlook.com>
2025-02-21 14:06:13 +0100

    finished L6: moving around

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 # this allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     # Set up display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids!")
     
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     # Main game loop
     while True:
         for event in pygame.event.get():
@@ -20,16 +26,15 @@ def main():
         
         # Fill the screen with a color
         screen.fill("black")
+        player.draw(screen)
         
+        player.update(dt)
+
         # Update the display
         pygame.display.flip()
         
         # Cap the frame rate to 60 frames per second
         dt = (clock.tick(60) / 1000)
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,6 +1,10 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
 class Player(CircleShape):
-    def __init__(self, x, y)
-        super().__init__(PLAYER_RADIUS)
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
     
     # in the player class
@@ -12,3 +16,16 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
+
+    def rotate(self, dt, direction=1):
+        self.rotation += PLAYER_TURN_SPEED * dt * direction
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt, direction=-1)
+        if keys[pygame.K_d]:
+            self.rotate(dt, direction = 1)
